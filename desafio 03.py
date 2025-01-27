c07e35fa5a96dca04607117eed40819fdd7827a3
Elvis Miranda <123380523+elvis28miranda@users.noreply.github.com>
2025-01-27 10:34:32 -0300

    Exibe a somatória de números simples

---
+++ desafio 03.py
@@ -0,0 +1,11 @@
+print("-------------------------------------------")
+numero01 = input ('digite o primeiro numero: ')
+print("-------------------------------------------")
+numero02 = input ('digite o segundo numero: ')
+
+resultado = int(numero01) + int(numero02)
+
+print("-------------------------------------------")
+print ('      O resultado é: ', resultado)
+print("-------------------------------------------")
+

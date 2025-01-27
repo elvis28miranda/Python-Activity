a38bb2511498a817d85d8d0ff16f4302d3e82d03
Elvis Miranda <123380523+elvis28miranda@users.noreply.github.com>
2025-01-27 10:31:36 -0300

    Pergunta e exibe data de aniversario

---
+++ desafio 02.py
@@ -0,0 +1,5 @@
+dia = input ('Que dia voce nasceu? ')
+mes = input ('Em Qual mes voce nasceu? ')
+ano = input ('Em Qual ano voce nasceu? ')
+
+print ('Voce nasceu no dia: ' + dia + ' de', mes + ' de', ano + ', correto?')
